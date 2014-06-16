a756d118b24e23b4a16f7acb7636973ea059fab4
Clay Molitor <ClayMolitor@Live.Com>
2014-06-15 22:56:57 -0700

    Created 3x3 grid for TTT game

---
+++ TicTacToe/src/android/widget/SquareLayout.java
@@ -0,0 +1,33 @@
+package android.widget;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SquareLayout extends RelativeLayout {
+    public SquareLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (width > (int)(1 * height + 0.5)) {
+            width = (int)(1 * height + 0.5);
+        } else {
+            height = (int)(width / 1 + 0.5);
+        }
+
+        super.onMeasure(
+                MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY)
+        );
+    }
+}
